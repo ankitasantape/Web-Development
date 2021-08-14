d729f166999d8f10a6f80ae66df40a6fd5dbb6ab
ankitasantape <ankitasantape99@gmail.com>
2021-08-14 21:48:20 +0530

    Updated main

--- JS/Module_3_WebScrapping/Activity/cricinfo/main.js
+++ JS/Module_3_WebScrapping/Activity/cricinfo/main.js
@@ -2,6 +2,11 @@ let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
 let request = require("request");
 let cheerio = require("cheerio");
 let scoreCardObj=require("./scoreCard")
+let fs = require("fs");
+let path = require("path");
+let iplPath = path.join(__dirname, "IPL");
+dirCreater(iplPath);
+
 // myTeamName	name	venue	date	opponentTeamName	result	runs	balls	fours	sixes	sr
 request(url, cb);
 function cb(error, response, html) {
@@ -27,6 +32,7 @@ function dataExtracter(html) {
     //  go to all match Page
     request(fullAllmatchPageLink, allMatchPageCb);
 }
+
 function allMatchPageCb(error, response, html) {
     if (error) {
         console.log(error); // Print the error if one occurred
@@ -38,6 +44,7 @@ function allMatchPageCb(error, response, html) {
         getAllScoreCardLink(html);
     }
 }
+
 function getAllScoreCardLink(html) {
     console.log("```````````````````````");
     let searchTool = cheerio.load(html);
@@ -45,9 +52,16 @@ function getAllScoreCardLink(html) {
     for (let i = 0; i < scorecardsArr.length; i++) {
         let link = searchTool(scorecardsArr[i]).attr("href");
         let fullAllmatchPageLink= `https://www.espncricinfo.com${link}`;
-        console.log(fullAllmatchPageLink);
+        // console.log(fullAllmatchPageLink);
         scoreCardObj.processSinglematch(fullAllmatchPageLink)
 
     }
     console.log("`````````````````");
+
+}
+
+function dirCreater(filePath){
+      if (fs.existsSync(filePath) == false){
+          fs.mkdirSync(filePath);
+      }   
 }
