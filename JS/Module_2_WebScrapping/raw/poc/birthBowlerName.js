8970312508b37106989f9e3779199208f5845df4
ankitasantape <ankitasantape99@gmail.com>
2021-08-29 17:00:58 +0530

    Added sorted birthday file of bowlers

--- JS/Module_2_WebScrapping/raw/poc/birthBowlerName.js
+++ JS/Module_2_WebScrapping/raw/poc/birthBowlerName.js
@@ -4,6 +4,8 @@ let cheerio = require("cheerio");
 let fs = require("fs");
 console.log("Before");
 
+let bowlersArr = [];
+let bowlersCount = 0;
 let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/royal-challengers-bangalore-vs-sunrisers-hyderabad-eliminator-1237178/full-scorecard";
 request(url , cb);
 function cb(error, response, html){
@@ -33,18 +35,29 @@ function dataExtracter(html){
    for (let i = 0; i < bowlers.length; i++){
         // row -> col
         let cols = searchTool(bowlers[i]).find("td");
+        if ( cols.length > 1)
+        {
+            bowlersCount++;
+        }
+   }
+   for (let i = 0; i < bowlers.length; i++){
+       // row -> col
+       let cols = searchTool(bowlers[i]).find("td"); 
+       if ( cols.length > 1)
+        { 
         // find the anchor tag which gives link
         let aElem = searchTool(cols[0]).find("a");
         let link = aElem.attr("href");
-        // link 
-        // new page -> link get -> complete -> request
+        // // link 
+        // // new page -> link get -> complete -> request
         let fullLink = `https://www.espncricinfo.com${link}`;
-        request(fullLink, newbc);
+        request(fullLink, newcb);
+        }
    }
    
 }
 
-function newbc(error, response, html){
+function newcb(error, response, html){
     if (error) {
         console.log(error);
     } else if (response.statusCode == 404){
@@ -52,17 +65,97 @@ function newbc(error, response, html){
     } else {
         // console.log(html); // Print the HTML 
         getBirthDay(html);
+
+        if(bowlersArr.length == bowlersCount){
+            console.table(bowlersArr);
+
+            sortBirthDay(bowlersArr);
+        }
     } 
 }
 
 function getBirthDay(html){
     let searchTool = cheerio.load(html);
     let headingsArr = searchTool(".player-card-description");
-    let age = searchTool(headingsArr[2]).text();
     let name = searchTool(headingsArr[0]).text();
-    console.log(name, " ", age);
+    let age = searchTool(headingsArr[2]).text();
+    
+    // console.log(name, " ", age);
+    bowlersArr.push({"name": name, "age":age});
 } 
 console.log("After");
+
+function sortBirthDay(bowlersArr){
+    // sort
+    // age -> convert
+    let newArr = bowlersArr.map(singleFn);
+    // console.log("newArr", newArr);
+    function singleFn(obj){
+        let name = obj.name;
+        let age = obj.age;
+        let ageArr = obj.age.split(" ");
+        let years = ageArr[0].slice(0, ageArr[0].length - 1); 
+        let days = ageArr[1].slice(0, ageArr[1].length - 1); 
+        let ageInDays = Number(years) * 365 + Number(days);
+        return{
+            name: name,
+            ageInDays: ageInDays,
+            age: age
+        }
+    }
+
+    let sortedArr = newArr.sort(cb);
+    // console.table(sortedArr);
+    function cb(objA, objB){
+        return objA.ageInDays - objB.ageInDays;
+    }
+    let finalArr = sortedArr.map(removeAgeInDays);
+    function removeAgeInDays(obj){
+        return {
+            name: obj.name,
+            age: obj.age
+        }
+    }
+    console.table(finalArr);
+
+}
+
+/*
+Before
+After
+┌─────────┬──────────────────────────┬────────────┐
+│ (index) │           name           │    age     │
+├─────────┼──────────────────────────┼────────────┤
+│    0    │    'Moeen Munir Ali'     │ '34y 72d'  │
+│    1    │ 'Yuzvendra Singh Chahal' │ '31y 37d'  │
+│    2    │      'Shivam Dube'       │ '28y 64d'  │
+│    3    │     'Sandeep Sharma'     │ '28y 103d' │
+│    4    │  'Thangarasu Natarajan'  │ '30y 94d'  │
+│    5    │   'Washington Sundar'    │ '21y 328d' │
+│    6    │     'Mohammed Siraj'     │ '27y 169d' │
+│    7    │     'Shahbaz Nadeem'     │ '32y 17d'  │
+│    8    │   'Rashid Khan Arman'    │ '22y 343d' │
+│    9    │       'Adam Zampa'       │ '29y 151d' │
+│   10    │ 'Navdeep Amarjeet Saini' │ '28y 279d' │
+│   11    │   'Jason Omar Holder'    │ '29y 297d' │
+└─────────┴──────────────────────────┴────────────┘
+┌─────────┬──────────────────────────┬────────────┐
+│ (index) │           name           │    age     │
+├─────────┼──────────────────────────┼────────────┤
+│    0    │   'Washington Sundar'    │ '21y 328d' │
+│    1    │   'Rashid Khan Arman'    │ '22y 343d' │
+│    2    │     'Mohammed Siraj'     │ '27y 169d' │
+│    3    │      'Shivam Dube'       │ '28y 64d'  │
+│    4    │     'Sandeep Sharma'     │ '28y 103d' │
+│    5    │ 'Navdeep Amarjeet Saini' │ '28y 279d' │
+│    6    │       'Adam Zampa'       │ '29y 151d' │
+│    7    │   'Jason Omar Holder'    │ '29y 297d' │
+│    8    │  'Thangarasu Natarajan'  │ '30y 94d'  │
+│    9    │ 'Yuzvendra Singh Chahal' │ '31y 37d'  │
+│   10    │     'Shahbaz Nadeem'     │ '32y 17d'  │
+│   11    │    'Moeen Munir Ali'     │ '34y 72d'  │
+└─────────┴──────────────────────────┴────────────┘
+*/ 
 
 // Order doesn't matter - order always changing  
 
