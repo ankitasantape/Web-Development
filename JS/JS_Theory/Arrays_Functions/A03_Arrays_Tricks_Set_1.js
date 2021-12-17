e8420bd71e0662b0439780f26c93eedd1f8d1f6f
ankitasantape <ankitasantape99@gmail.com>
2021-12-17 18:36:25 +0530

    updated files

--- JS/JS_Theory/Arrays_Functions/A03_Arrays_Tricks_Set_1.js
+++ JS/JS_Theory/Arrays_Functions/A03_Arrays_Tricks_Set_1.js
@@ -1,17 +1,18 @@
 
 
 let arr = [20, 30, 40, 50, 60];
-displayArray(arr);
+displayArray(arr);  // 20,30,40,50,60 = 5
 
 let rv = arr.pop();
-displayArray(arr);
-console.log(rv); 
+displayArray(arr); // 20,30,40,50 = 4
+console.log(rv);  // 60
 
 arr[50] = 100;
-displayArray(arr);
+displayArray(arr); // 20,30,40,50,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,100 = 51
 
 arr["kuchbhi"] = 100;
-console.log(arr["kuchbhi"]);
+console.log(arr["kuchbhi"]); // 100
+displayArray(arr); // 20,30,40,50,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,100 = 51
 
 function displayArray(arr){
     console.log(arr + " = " + arr.length);
---
+++ JS/JS_Theory/Arrays_Functions/A04_Arrays_Shift.js
@@ -0,0 +1,10 @@
+// shift removes 1 value from the front 
+// shift returns the removed value
+
+let arr = [10, 20, 30, 40, 50];
+displayArray(arr);
+
+
+function displayArray(arr){
+    console.log(arr + " = " + arr.length);
+}
